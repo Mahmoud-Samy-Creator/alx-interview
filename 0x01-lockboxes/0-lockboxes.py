159732181f227f9616c12f431fb2cc30da940d4d
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-21 19:44:25 +0200

    Task [0]

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,8 +5,9 @@
 def canUnlockAll(boxes):
     if type(boxes) is not list or len(boxes) == 0:
         return False
-    keys = [0]
-    for i in range(0, len(boxes)):
+    keys = []
+    keys.extend(boxes[0])
+    for i in range(1, len(boxes)):
         if i in keys:
             keys.extend(boxes[i])
             for j in boxes[i]:

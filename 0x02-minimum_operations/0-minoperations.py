4a73471eef587cdd34414c7d9d450235e4ab6ecf
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2024-04-26 15:55:52 +0300

    task [0]

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,8 +7,6 @@ def minOperations(n):
     calculates the fewest number of operations needed
     """
     num, sum = n, 0
-    if num is not isinstance(int):
-        return sum
     while (num > 1):
         if num % 2 == 0:
             sum += 2
